ba25fe67e028b38baea860cc12bedc4b5619b8f3
Unbegabt 83 <Chris.wesch@hotmail.com>
2018-01-17 18:32:22 +0100

    Initial Commit

--- Program.cs
+++ Program.cs
@@ -8,8 +8,76 @@ namespace CSharpSlotMaschine
 {
     class Program
     {
+        //Variablen
+        public static int NumberA = 0;
+        public static int NumberB = 0;
+        public static int NumberC = 0;
+        public static int Token = 2;
+
+        //Erstellen der random Funktion
+        static Random random = new Random();
+
+
+        //Hier beginnt dann die "Main"
         static void Main(string[] args)
         {
+
+            Game();
+
+
+            while (Token > 0)
+            {
+                Console.WriteLine("Weiterspielen? (J)a oder (N)ein?");
+                ConsoleKeyInfo info = Console.ReadKey();
+                Console.CursorLeft = 0;
+
+                if (info.Key == ConsoleKey.J)
+                {
+                    Game();
+                }
+                else if (info.Key == ConsoleKey.N)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Herzlichen Glückwunsch, sie haben " + Token + " Token erspielt!");
+                    Console.WriteLine("Nicht alles auf einmal Ausgeben");
+                    Console.ReadKey();
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("Falsche Eingabe, bitte nur (J) für Ja oder (N) für Nein!");
+
+                }
+
+            }
+            Console.Clear();
+            Console.WriteLine(NumberA + " " + NumberB + " " + NumberC);
+            Console.WriteLine("Schade, keine Token mehr");
+            Console.ReadKey();
+        }
+
+        private static void Game()
+        {
+            //Randomzahlen in A B und C setzen
+            NumberA = random.Next(1, 10);
+            NumberB = random.Next(1, 10);
+            NumberC = random.Next(1, 10);
+
+            // Ausgabe der Zahlen in einer Reihe
+            Console.WriteLine(NumberA + " " + NumberB + " " + NumberC);
+
+            if (NumberA == NumberB && NumberA == NumberC)
+            {
+                Token = Token + 10;
+            }
+            else if (NumberA == NumberB || NumberB == NumberC || NumberA == NumberC)
+            {
+                Token = Token + 5;
+            }
+            else
+                Token = Token - 1;
+
+            Console.WriteLine("Du hast jetzt " + Token + " Token");
         }
     }
 }
